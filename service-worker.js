774f1a6481cb22aa033ff47044606d0f6ef72fd7
mathyf <hyf20010101@outlook.com>
2026-05-22 12:30:45 +0000

    Deploying to gh-pages-master from  @ 124772087cf03f0f546bc5a212cbccabaa8c7247 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox/workbox-sw.js");
 
 importScripts(
-  "https://cdn.jsdelivr.net/gh/treehollow/webhole@gh-pages-master/precache-manifest.3309145b645301b7c0804e2a59dbf7c5.js"
+  "https://cdn.jsdelivr.net/gh/euhollow/webhole@gh-pages-master/precache-manifest.b64e211111d4fdb373c2356afc8c4014.js"
 );
 
 self.addEventListener('message', (event) => {
